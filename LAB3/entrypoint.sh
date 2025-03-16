c3fe29db1b248b7eae649e48527c9bb625afe961
Kim Dong Hyun <kimdonghyun0916@gmail.com>
2025-03-16 22:57:33 +0900

    Update entrypoint.sh

--- LAB3/entrypoint.sh
+++ LAB3/entrypoint.sh
@@ -8,9 +8,9 @@ sleep 30
 
 #MongoDB connection variables
 MONGO_PORT="27017"
-MONGO_DB="WCD_project2"
-COLLECTION_NAME="nhl_stats_2022"
-CSV_FILE="nhl-stats-2022.csv"
+MONGO_DB="LAB2"
+COLLECTION_NAME="KBL_Pitcher_2024"
+CSV_FILE="KBL-Pitcher-2024.csv"
 
 #Import the CSV file into the MongoDB collection
 mongoimport --host db --port $MONGO_PORT --db $MONGO_DB --collection $COLLECTION_NAME --type csv --headerline --file $CSV_FILE
